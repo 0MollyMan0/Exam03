0f2cc464a27dcda9d8d3203020f5b77e30b4f606
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-04 10:29:53 +0100

    add is_valid

--- backtracking/bit_pos.c
+++ backtracking/bit_pos.c
@@ -1,17 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   bit_pos.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 09:37:47 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/04 09:52:50 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/04 10:10:01 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int is_valid(int *tab, int pos)
+{
+	if (pos == 0)
+		return (1);
+	if (tab[pos] == 1 && tab[pos - 1] == 1)
+		return 0;
+	return (1);
+}
 
 void	bit_pos(int pos, int len, int *tab)
 {
@@ -29,7 +38,8 @@ void	bit_pos(int pos, int len, int *tab)
 	while (choix <= 1)
 	{
 		tab[pos] = choix;
-		bit_pos(pos + 1, len, tab);
+		if (is_valid(tab, pos))
+			bit_pos(pos + 1, len, tab);
 		choix++;
 	}
 }
---
+++ backtracking/n_queens.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   n_queens.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/04 10:29:16 by anfouger          #+#    #+#             */
+/*   Updated: 2026/01/04 10:29:29 by anfouger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_valid(int *tab, int pos)
+{
+	if (pos == 0)
+		return (1);
+	if (tab[pos] == 1 && tab[pos - 1] == 1)
+		return 0;
+	return (1);
+}
+
+void	bit_pos(int pos, int len, int *tab)
+{
+	int choix;
+
+	if (pos == len)
+	{
+		for (int i = 0; i < len; i++)
+			printf("%d", tab[i]);
+		printf("%s", "\n");
+		return ;
+	}
+	
+	choix = 0;
+	while (choix <= 1)
+	{
+		tab[pos] = choix;
+		if (is_valid(tab, pos))
+			bit_pos(pos + 1, len, tab);
+		choix++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	int arr[atoi(av[1])];
+	
+	bit_pos(0, atoi(av[1]), arr);
+	return (0);
+}
