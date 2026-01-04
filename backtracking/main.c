4d1d8f3233dada1a3a87f2bc41d2c5f72b1688ff
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-04 09:54:51 +0100

    backtracking learning

---
+++ backtracking/main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/04 09:37:47 by anfouger          #+#    #+#             */
+/*   Updated: 2026/01/04 09:52:50 by anfouger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void	bit_pos(int pos, int len, int *tab)
+{
+	int choix;
+
+	if (pos == len)
+	{
+		for (int i = 0; i < len; i++)
+			printf("%d", tab[i]);
+		printf("%s", "\n");
+		return ;
+	}
+	
+	choix = 0;
+	while (choix <= 1)
+	{
+		tab[pos] = choix;
+		bit_pos(pos + 1, len, tab);
+		choix++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	int arr[atoi(av[1])];
+	
+	bit_pos(0, atoi(av[1]), arr);
+	return (0);
+}
