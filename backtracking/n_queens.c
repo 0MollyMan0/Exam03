a70487c6e02375191d1805650f339dc883acc9ea
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-10 13:11:39 +0100

    place n queens

--- backtracking/n_queens.c
+++ backtracking/n_queens.c
@@ -6,14 +6,14 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 10:29:16 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/10 12:42:14 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/10 13:10:59 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	display_board(int n, int x, int y)
+void	display_board(int n, int *queens)
 {
 	int row;
 	int col;
@@ -24,7 +24,7 @@ void	display_board(int n, int x, int y)
 		col = 0;
 		while (col < n)
 		{
-			if (y == row && x == col)
+			if (queens[row] == col)
 				printf("%s", " Q ");
 			else	
 				printf("%s", " . ");
@@ -35,10 +35,42 @@ void	display_board(int n, int x, int y)
 	}
 }
 
+int *place_queens(int n, int *queens)
+{
+	int row;
+	int col;
+	
+	row = 0;
+	while (row < n)
+	{
+		col = 0;
+		while (col < n)
+		{
+			if (col == row)
+				queens[row] = col;
+			col++;
+		}
+		row++;
+	}
+	return (queens);
+}
+
 int	main(int ac, char **av)
-{	
-	if (ac != 4)
+{
+	int x;
+	int y;
+	int n;
+	int *queens;
+	
+	x = 3;
+	y = 4;
+	if (ac != 2)
 		return (1);
-	display_board(atoi(av[1]), atoi(av[2]), atoi(av[3]));
+	n = atoi(av[1]);
+	queens = malloc(sizeof(int) * n);
+	if (!queens)
+		return (1);
+	place_queens(n, queens);
+	display_board(n, queens);
 	return (0);
 }
