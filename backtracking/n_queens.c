8f62004aca19ed2f3601c6c8ba419e59ba1b43ca
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-10 15:39:14 +0100

    n queens solved

--- backtracking/n_queens.c
+++ backtracking/n_queens.c
@@ -6,21 +6,57 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 10:29:16 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/10 13:57:35 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/10 15:36:43 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int	check_diag_right(int q_row, int q_col, int row, int col)
+{
+	int	diag_row;
+	int	diag_col;
+	
+	diag_row = q_row + 1;
+	diag_col = q_col + 1;
+	while (diag_row <= row)
+	{
+		if (diag_col == col && diag_row == row)
+			return(0);
+		diag_col++;
+		diag_row++;
+	}
+	return (1);
+}
+
+int	check_diag_left(int q_row, int q_col, int row, int col)
+{
+	int	diag_row;
+	int	diag_col;
+	
+	diag_row = q_row + 1;
+	diag_col = q_col - 1;
+	while (diag_col >= 0)
+	{
+		if (diag_col == col && diag_row == row)
+			return(0);
+		diag_col--;
+		diag_row++;
+	}
+	return (1);
+}
+
 int is_valid(int *queens, int row, int col)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < row)
 	{
-		if (queens[i] == col)
+		if (queens[i] == col
+			|| !check_diag_left(i, queens[i], row, col)
+			|| !check_diag_right(i, queens[i], row, col))
 			return (0);
 		i++;
 	}
@@ -49,7 +85,7 @@ void	display_board(int n, int *queens)
 	}
 }
 
-void	place_queens(int n, int row, int *queens)
+void	n_queens(int n, int row, int *queens)
 {
 	int col;
 
@@ -59,14 +95,13 @@ void	place_queens(int n, int row, int *queens)
 		printf("%s", "\n");
 		return ;
 	}
-	
 	col = 0;
 	while (col < n)
 	{
 		if (is_valid(queens, row, col))
 		{
 			queens[row] = col;
-			place_queens(n, row+1, queens);
+			n_queens(n, row+1, queens);
 		}
 		col++;
 	}
@@ -87,6 +122,6 @@ int	main(int ac, char **av)
 	queens = malloc(sizeof(int) * n);
 	if (!queens)
 		return (1);
-	place_queens(n, 0, queens);
+	n_queens(n, 0, queens);
 	return (0);
 }
