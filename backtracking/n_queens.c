945ad2654ac2b9cb22a5f5bd05179b2c3f1c6afb
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-10 12:45:09 +0100

    display 1 queen

--- backtracking/n_queens.c
+++ backtracking/n_queens.c
@@ -6,36 +6,39 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 10:29:16 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/10 12:35:28 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/10 12:42:14 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	display_board(int n)
+void	display_board(int n, int x, int y)
 {
-	int col;
 	int row;
+	int col;
 
-	col = 0;
-	while (col < n)
+	row = 0;
+	while (row < n)
 	{
-		row = 0;
-		while (row < n)
+		col = 0;
+		while (col < n)
 		{
-			printf("%s", " . ");
-			row++;
+			if (y == row && x == col)
+				printf("%s", " Q ");
+			else	
+				printf("%s", " . ");
+			col++;
 		}
 		printf("%s", "\n");
-		col++;
+		row++;
 	}
 }
 
 int	main(int ac, char **av)
-{
-	if (ac != 2)
+{	
+	if (ac != 4)
 		return (1);
-	display_board(atoi(av[1]));
+	display_board(atoi(av[1]), atoi(av[2]), atoi(av[3]));
 	return (0);
 }
