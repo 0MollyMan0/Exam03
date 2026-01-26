36f711e5825804797b636ffb28242e66538fab86
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-26 08:43:13 +0100

    read

--- fd/prog.c
+++ fd/prog.c
@@ -6,15 +6,25 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 08:17:30 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/26 08:21:44 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/26 08:37:14 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
-	write(1, "OK\n", 3);
-	write(2, "ERROR\n", 6);
+	char	c;
+	int		is_worked;
+
+	is_worked = read(0, &c, 1);
+	while (is_worked)
+	{
+		write(1, &c, 1);
+		is_worked = read(0, &c, 1);	
+	}
+	if (is_worked == -1)
+		perror("error");
 	return (0);
 }
